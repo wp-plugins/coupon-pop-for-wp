c90be86c661d1167558b79d014a4cf1fcc4e617c
storeya <storeya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 11:14:18 +0000

    wp version fix
    git-svn-id: https://plugins.svn.wordpress.org/coupon-pop-for-wp/tags@823051 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.3/readme.txt
@@ -0,0 +1,66 @@
+=== Coupon Pop widget for WP ===
+Contributors: StoreYa
+Tags: Facebook, ecommerce, import, Viral marketing, woocommerce, woo-commerce, wp-commerce, wp, Rewards, Facebook marketing, SocialMarketing, Online Sales, Marketing tools, Social commerce, storeya, Promotional tool, engagement, Online  marketing, EmailMarketing, Twitter, G+, google, google plus, Instagram, Pinterest, LinkedIn, likes, get likes, buy likes, fans, add fans, buyfans, Incentive app, Social campaigns, ads, seo, ppc, wordpress, share, blog, plugin, sharing, network, tumblr, coupon, email, facebook likes, get likes, increase sales, likes, twitter followers, deals, discount, sales, widget, token,  paypal
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.3
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin that increases your fan base and email lists by popping up special offers and discounts to your visitors.
+
+== Description ==
+
+StoreYa Coupon Pop Plugin boosts your website conversion rate and increases your fan base and email lists by popping up special offers and discounts to your visitors.
+
+Coupon Pop enables you to pre-define a promotion that would pop up to your visitors asking them to join you in any social network you are active or wish to be active on as well as your to your emails list.
+Coupon pop works with Facebook, Twitter, Google+, LinkedIn, and has an easy one minute set up!
+
+Add the Coupon Pop to your site Today, and you'll be amazed from the quick results!!
+
+**NEW!** Coupon Pop now comes with a Christmas Theme:-)
+
+To learn more about StoreYa and how it works visit [our website](http://www.storeya.com/public/couponpop).
+
+== Installation ==
+
+1. Download the  zip file and save it locally - 'storeya-coupon-pop.zip'.
+
+2. Log in to your WordPress administration screen.
+
+3. Click on 'Plugins' and then on 'Add New' (left menu).
+
+4. Click on the 'Upload' link, choose 'storeya-coupon-pop.zip' file and click on the 'Install' now button.
+
+5. Click on 'Activate Plugin' link
+
+6. Click on the StoreYa Coupon Pop > Settings
+
+8. Go to www.storeya.com/public/couponpop
+
+9. If you are not logged in, please click on the "Create a Free Coupon Pop Now!" button.
+
+10. Once you are happy with your Coupon Pop customization, copy the code and paste it to the Coupon Pop for WP setting screen. 
+
+11. Click 'Save changes'.
+
+== Screenshots ==
+
+1. Christmas Theme Coupon Pop.
+
+2. Coupon Pop - Free Christmas package.
+
+3. Coupon Pop - Live example.
+
+4. Coupon Pop is compatible to Desktop, Tablet, and mobile.
+
+5. Coupon Pop with all the social networks turned on.
+
+6. Gallery Carousel.
+
+== Resources ==
+
+[StoreYa support](https://storeya.zendesk.com/home)
+
+[Coupon Pop website](http://www.storeya.com/public/couponpop)
+
---
+++ 1.3/storeya-coupon-pop.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Coupon Pop widget for WP
+Plugin URI: http://www.storeya.com/public/couponpop
+Description: A plugin that increases your fan base and email lists by popping up special offers and discounts to your visitors.
+Version: 1.1
+Author: StoreYa
+Author URI: http://www.storeya.com/
+
+=== VERSION HISTORY ===
+01.11.13 - v1.0 - The first version
+
+=== LEGAL INFORMATION ===
+Copyright © 2013 StoreYa Feed LTD - http://www.storeya.com/
+
+License: GPLv2 
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$plugurldir = get_option('siteurl') . '/' . PLUGINDIR . '/storeya-coupon-pop/';
+$scp_domain = 'StoreYaCouponPop';
+load_plugin_textdomain($scp_domain, 'wp-content/plugins/storeya-coupon-pop');
+add_action('init', 'scp_init');
+add_action('wp_footer', 'scp_insert');
+add_action('admin_notices', 'scp_admin_notice');
+add_filter('plugin_action_links', 'scp_plugin_actions', 10, 2);
+
+function scp_init()
+{
+    if (function_exists('current_user_can') && current_user_can('manage_options'))
+        add_action('admin_menu', 'scp_add_settings_page');
+    if (!function_exists('get_plugins'))
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    $options = get_option('scpDisable');
+}
+function scp_settings()
+{
+    register_setting('storeya-coupon-pop-group', 'scpID');
+    register_setting('storeya-coupon-pop-group', 'scpDisable');
+    add_settings_section('storeya-coupon-pop', "StoreYa Coupon Pop", "", 'storeya-coupon-pop-group');
+
+}
+function scp_plugin_get_version()
+{
+    if (!function_exists('get_plugins'))
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    $plugin_folder = get_plugins('/' . plugin_basename(dirname(__FILE__)));
+    $plugin_file   = basename((__FILE__));
+    return $plugin_folder[$plugin_file]['Version'];
+}
+function scp_insert()
+{
+    global $current_user;
+    if (get_option('scpID')) {
+            
+        $script = str_replace("\"","\"",get_option('scpID'));
+        echo $script; 
+    }
+}
+
+function scp_admin_notice()
+{
+    if (!get_option('scpID'))
+        echo ('<div class="error"><p><strong>' . sprintf(__('StoreYa Coupon Pop  is disabled. Please go to the <a href="%s">plugin page</a> and enter a valid Couopon Pop script to enable it.'), admin_url('options-general.php?page=storeya-coupon-pop')) . '</strong></p></div>');
+}
+function scp_plugin_actions($links, $file)
+{
+    static $this_plugin;
+    if (!$this_plugin)
+        $this_plugin = plugin_basename(__FILE__);
+    if ($file == $this_plugin && function_exists('admin_url')) {
+        $settings_link = '<a href="' . admin_url('options-general.php?page=storeya-coupon-pop') . '">' . __('Settings', $scp_domain) . '</a>';
+        array_unshift($links, $settings_link);
+    }
+    return ($links);
+}
+
+    function scp_add_settings_page()
+    {
+        function scp_settings_page()
+        {
+            global $scp_domain, $plugurldir, $storeya_options;
+?>
+      <div class="wrap">
+        <?php
+            screen_icon();
+?>
+        <h2><?php
+            _e('StoreYa Coupon Pop ', $scp_domain);
+?> <small><?
+            echo scp_plugin_get_version();
+?></small></h2>
+        <div class="metabox-holder meta-box-sortables ui-sortable pointer">
+          <div class="postbox" style="float:left;width:30em;margin-right:20px">
+            <h3 class="hndle"><span><?php
+            _e('StoreYa Coupon Pop  - Settings', $scp_domain);
+?></span></h3>
+            <div class="inside" style="padding: 0 10px">
+              <p style="text-align:center">
+		      </p>
+              <form method="post" action="options.php">
+                <?php
+            settings_fields('storeya-coupon-pop-group');
+?>
+                <p><label for="scpID"><?php
+            printf(__('
+Enter Coupon Pop script you got from %1$sIncrease your online sales today with StoreYa!%2$sStoreYa%3$s.', $scp_domain), '<strong><a href="http://www.storeya.com/public/couponpop" target="_blank"  title="', '">', '</a></strong>');
+?></label></p>
+
+                  <p><textarea rows="11" cols="62" name="scpID" ><?php echo get_option('scpID');?></textarea></p>
+                    <p class="submit">
+                      <input type="submit" class="button-primary" value="<?php
+            _e('Save Changes');
+?>" />
+                    </p>
+                  </form>
+</p>
+                  <p style="font-size:smaller;color:#999239;background-color:#ffffe0;padding:0.4em 0.6em !important;border:1px solid #e6db55;-moz-border-radius:3px;-khtml-border-radius:3px;-webkit-border-radius:3px;border-radius:3px"><?php
+            printf(__('Don&rsquo;t have a Coupon Pop? No problem! %1$sKeep your visitors engaged with you in all social networks you are active on!%2$sCreate a <strong>FREE</strong> StoreYa Coupon Pop  Now!%3$s', $scp_domain), '<a href="http://www.storeya.com/public/couponpop" target="_blank" title="', '">', '</a>');
+?></p>
+                  </div>
+                </div>
+
+
+
+
+                </div>
+              </div>
+			  <img src="http://www.storeya.com/widgets/admin/wpcouponpop"/>
+              <?php
+        }
+        add_action('admin_init', 'scp_settings');
+        add_submenu_page('options-general.php', __('StoreYa Coupon Pop ', $scp_domain), __('StoreYa Coupon Pop ', $scp_domain), 'manage_options', 'storeya-coupon-pop', 'scp_settings_page');
+    }
+
+?>
